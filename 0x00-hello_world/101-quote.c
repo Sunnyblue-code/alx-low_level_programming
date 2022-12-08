0c1f735f405052f5b0f00944b495d559c5ce9b8c
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-08 16:05:07 +0100

    print statement

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity */
+     
+    int main(){
+    
+    printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+
+    return (0);
+    
+    }
