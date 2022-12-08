77b86dfe78e03f6a5748e6585d799f41addf8706
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-08 16:08:48 +0100

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
-/* UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity */
-     
-    int main(){
-    
-    printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
 
-    return (0);
-    
-    }
+int main(void)
+{
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
+}
