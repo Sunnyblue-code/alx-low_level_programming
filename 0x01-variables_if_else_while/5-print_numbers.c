0ce100207fefdd43d9b2b252ab1b12f0a30e7126
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-09 16:12:59 +0100

    job done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: prints numbers from 1 to 10
+ * Return: 0
+*/
+int main(void){
+    int base;
+    for (base = 0; base <= 10; base++)
+{
+        printf("%d\n", base);
+}
+    
+    return (0);
+}
