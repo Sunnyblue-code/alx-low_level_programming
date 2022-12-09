038b8703ce3283bf7ad7b61a3a5e8dc75850f781
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-09 14:52:43 +0100

    lower alphabet number a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+# include <conio.h>
+# include <stdio.h>
+# include <stdlib.h>
+/** program that print small alphabets */
+int main(){
+    
+    int num;
+    for ( num = 'a'; num < 'z'; num++)
+    {
+        /* code */
+        putchar(num);
+        putchar('\n');
+    }
+    
+
+    
+    return 0;
+}
