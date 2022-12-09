a1cef4f7f5f71c6448e71aed05981cc29aa10cce
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-09 15:09:18 +0100

    print alphabet in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
-# include <stdio.h>
-# include <stdlib.h>
-/** program that print small alphabets */
-int main(){
-    
-    int num;
-    for ( num = 'a'; num < 'z'; num++)
-    {
-        /* code */
-        putchar(num);
-        putchar('\n');
-    }
-    
-
-    
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: prints alphabet in lowercase
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+		for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
 }
