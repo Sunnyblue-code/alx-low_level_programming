6ef41e101506f1bfdc1558c2a30f46101ea043fa
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-13 21:03:57 +0100

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+OBOBOB
+/**
+OBOBOB * _abs - compute absolute value of integer
+ * @n: integer argument
+ * Return: absolute value
+ */
+OAOAOA
+OAOAOAint _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
+}
