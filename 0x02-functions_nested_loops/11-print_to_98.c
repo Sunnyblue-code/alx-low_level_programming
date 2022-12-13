71b7914433dbffcd788f554460a8c9cd381aaf70
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-13 21:17:26 +0100

    battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * add - Adds two integers.
- * @a: The first integer to be added.
- * @b: The second integer to be added.
- *
- * Return: a + b
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
  */
-int add(int a, int b)
+
+void print_to_98(int n)
 {
-	return (a + b);
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+		printf("%d", n);
+
+	printf("\n");
 }
