b69f29fbf09e211cd86e084b1fdc17c04d9aa1ce
Sunnyblue-code <sunnyexprez@gmail.com>
2022-12-22 13:32:21 +0100

    string 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,23 @@
+#include "main.h"
 #include <stdio.h>
-
-int main(void)
+/**
+  * print_number - prints number with _putchar
+  * @n: input number
+  * Return: returns void
+ **/
+void print_number(int n)
 {
-  int n;
-  int a[5];
-  int *p;
+	unsigned int number;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	number = n;
+	if (n < 0)
+	{
+	_putchar('-');
+	number = -n;
+	}
+	if (number / 10 != 0)
+	{
+	print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
 }
